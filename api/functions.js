bb8bbd256bc662d314aa234fab054790e521f3d1
Gisella <gisellamassiero@gmail.com>
2026-07-07 21:23:29 -0300

    fix: correccion de bug

--- api/functions.js
+++ api/functions.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
 
       const modelName = "gemini-1.5-flash"; 
 const response = await fetch(
-  `https://generativelanguage.googleapis.com/v1beta/models/${modelName}:generateContent?key=${process.env.GEMINI_API_KEY}`,
+ `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
   {
     method: "POST",
     headers: { "Content-Type": "application/json" },
